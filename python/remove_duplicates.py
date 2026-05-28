7ee628ce837f1d95e637534927071d4950d75b93
Shridhar Bhat <141147285+shridhar618@users.noreply.github.com>
2026-05-28 20:15:57 +0530

    main

---
+++ python/remove_duplicates.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        k=1
+
+        if len(nums)==0:
+            return 0
+
+        for i in range(1,len(nums)):
+            if nums[i]!=nums[k-1]:
+                nums[k]=nums[i]
+                k+=1
+
+        return k
+        
