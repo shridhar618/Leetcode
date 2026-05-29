83a6de7b90633413a41ecc14ad85b58dc90b950e
Shridhar Bhat <141147285+shridhar618@users.noreply.github.com>
2026-05-30 00:28:56 +0530

    init

---
+++ python/length-of-last-word.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        i=len(s)-1
+        count=0
+        while i>=0 and s[i]==" ": 
+            i-=1
+        while i>=0 and s[i]!=" ":
+            count+=1
+            i-=1
+
+        return count
+        
