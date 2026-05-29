2b56656c80f09b6ce9db526c57867a727b528a09
Shridhar Bhat <141147285+shridhar618@users.noreply.github.com>
2026-05-30 00:29:47 +0530

    init

---
+++ python/search_insert_position.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        for num in nums:
+            if num>=target:
+                return 
+
+        return len(nums)
+        
