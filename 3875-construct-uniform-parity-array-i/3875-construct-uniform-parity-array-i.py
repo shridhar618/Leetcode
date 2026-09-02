0ee68cc0818a0503c655392190f9ad41b2b341c4
Shridhara <141147285+shridhar618@users.noreply.github.com>
2026-09-02 11:05:43 +0530

    Time: 0 ms (100%), Space: 19.1 MB (84.34%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.py
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.py
@@ -1,3 +1,15 @@
 class Solution:
     def uniformArray(self, nums1: list[int]) -> bool:
-        return True
+        odd=0
+        even=0
+        for num in nums1:
+            if num %2==0:
+                even+=1
+            else:
+                odd+=1
+        if even == len(nums1) or odd == len(nums1):
+            return True
+
+        elif even>=1 and odd>=1:
+            return True
+        return False
