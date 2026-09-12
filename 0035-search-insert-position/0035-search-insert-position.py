84f03ddfe289202c33e45418c211b69d30fbf4b8
Shridhara <141147285+shridhar618@users.noreply.github.com>
2026-09-12 22:51:03 +0530

    Time: 0 ms (100%), Space: 12.8 MB (66.45%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -5,11 +5,12 @@ class Solution(object):
 
         while left<=right:
             mid=(left+right)//2
-            if target==nums[mid]:
+            if nums[mid]==target:
                 return mid
-            elif target>nums[mid]:
+            elif nums[mid]<target:
                 left=mid+1
-            elif target<nums[mid]:
+            elif nums[mid]>target:
                 right=mid-1
 
         return left
+
