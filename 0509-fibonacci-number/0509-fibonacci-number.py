1149f2db8e524eba279bec4f9359d75cfdb1e104
Shridhara <141147285+shridhar618@users.noreply.github.com>
2026-09-21 19:53:51 +0530

    Time: 596 ms (35.19%), Space: 12.5 MB (18.16%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,8 +1,6 @@
-class Solution:
-    def fib(self, n: int) -> int:
+class Solution(object):
+    def fib(self, n):
         if n<=1:
             return n
-
         return self.fib(n-1)+self.fib(n-2)
-
         
